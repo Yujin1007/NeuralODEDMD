e3e8c40cfd021795e03a04a2fa46fda9cf628654
Yujin1007 <yujin1004k@gmail.com>
2025-11-14 10:51:01 -0500

    remove src

--- src/get_save_dir.py
+++
@@ -1,4 +0,0 @@
-from config.config import Gray_Scott_Config
-
-cfg = Gray_Scott_Config()
-print(cfg.save_dir)
--- src/run_navier_stokes.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-SAVE_DIR=$(python utils/get_save_dir.py)
-echo $SAVE_DIR
-python train_navier_stokes.py
-python eval_navier_stokes.py --config_dir "$SAVE_DIR"
